3adce9de6b308b5d58d599b7d7127379abf0692f
Chia-Ying Ho <42313352+iosum@users.noreply.github.com>
2020-04-29 16:15:52 -0400

    delete data

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -4,19 +4,21 @@ import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import './custom.css'
 import { Trips } from './components/trips/Trips';
-import {Create} from './components/trips/Create';
-import {Update} from './components/trips/Update';
+import { Create } from './components/trips/Create';
+import { Update } from './components/trips/Update';
+import { Delete } from './components/trips/Delete';
 
 export default class App extends Component {
   static displayName = App.name;
 
-  render () {
+  render() {
     return (
       <Layout>
         <Route exact path='/' component={Home} />
-        <Route path='/create' component={Create}/>
-        <Route path='/trips' component={Trips}/>
+        <Route path='/create' component={Create} />
+        <Route path='/trips' component={Trips} />
         <Route path='/update/:id' component={Update} />
+        <Route path='/delete/:id' component={Delete} />
       </Layout>
     );
   }
--- ClientApp/src/components/trips/Trips.jsx
+++ ClientApp/src/components/trips/Trips.jsx
@@ -2,111 +2,118 @@ import React, { Component } from "react";
 import axios from "axios";
 
 export class Trips extends Component {
-         // use props to pass data to the view
-         constructor(props) {
-           super(props);
+  // use props to pass data to the view
+  constructor(props) {
+    super(props);
 
-            this.onTripUpdate = this.onTripUpdate.bind(this);
+    this.onTripUpdate = this.onTripUpdate.bind(this);
+    this.onTripDelete = this.onTripDelete.bind(this);
+    this.state = {
+      // store a list of trips
+      trips: [],
+      // check if the data is loaded or not
+      loading: true,
+    };
+  }
 
-           this.state = {
-             // store a list of trips
-             trips: [],
-             // check if the data is loaded or not
-             loading: true,
-           };
-         }
+  // get the api endpoint, we are going to send a request to get all the trips once the views are loaded
+  componentDidMount() {
+    this.populateTripsData();
+  }
 
-         // get the api endpoint, we are going to send a request to get all the trips once the views are loaded
-         componentDidMount() {
-           this.populateTripsData();
-         }
+  onTripUpdate(id) {
+    const { history } = this.props;
+    history.push("/update/" + id);
+  }
+  // add a delete method to the all trips view
+  onTripDelete(id) {
+    const { history } = this.props;
+    history.push("/delete/" + id);
+  }
 
-         onTripUpdate(id) {
-             const {history} = this.props;
-             history.push('/update/' + id);
-         }
+  populateTripsData() {
+    // send a get request to the api endpoint
+    axios.get("api/Trips/GetTrips").then((result) => {
+      // get all the trips from the data
+      const response = result.data;
+      // change the trip state to response, and close the loading
+      this.setState({ trips: response, loading: false });
+    });
 
-         populateTripsData() {
-           // send a get request to the api endpoint
-           axios.get("api/Trips/GetTrips").then((result) => {
-             // get all the trips from the data
-             const response = result.data;
-             // change the trip state to response, and close the loading
-             this.setState({ trips: response, loading: false });
-           });
+    // change the state of the trips to that result
+  }
 
-           // change the state of the trips to that result
-         }
-
-         // use state to render the table
-         renderAllTripsTable(trips) {
-           return (
-             <table className="table table-striped table-hover">
-               <thead>
-                 <th>Name</th>
-                 <th>Description</th>
-                 <th>Date Started</th>
-                 <th>Date Completed</th>
-                 <th>Actions</th>
-               </thead>
-               <tbody>
-                 {
-                   // each table row needs an unique identifier so we need to set a key
-                   trips.map((trip) => (
-                     <tr key={trip.id}>
-                       <td>{trip.name}</td>
-                       <td>{trip.description}</td>
-                       {/* when we don't have a date, we need to display a dash */}
-                       <td>
-                         {new Date(trip.dateStarted).toISOString().slice(0, 10)}
-                       </td>
-                       {/* first check if we have a date, if yes, display the date, otherwise, display the dash */}
-                       <td>
-                         {trip.dateCompleted
-                           ? new Date(trip.dateCompleted)
-                               .toISOString()
-                               .slice(0, 10)
-                           : "-"}
-                       </td>
-                       <td>
-                         <div className="form-group">
-                           {/* when clicking the update button, it will trigger the onClick method
+  // use state to render the table
+  renderAllTripsTable(trips) {
+    return (
+      <table className="table table-striped table-hover">
+        <thead>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Date Started</th>
+          <th>Date Completed</th>
+          <th>Actions</th>
+        </thead>
+        <tbody>
+          {
+            // each table row needs an unique identifier so we need to set a key
+            trips.map((trip) => (
+              <tr key={trip.id}>
+                <td>{trip.name}</td>
+                <td>{trip.description}</td>
+                {/* when we don't have a date, we need to display a dash */}
+                <td>{new Date(trip.dateStarted).toISOString().slice(0, 10)}</td>
+                {/* first check if we have a date, if yes, display the date, otherwise, display the dash */}
+                <td>
+                  {trip.dateCompleted
+                    ? new Date(trip.dateCompleted).toISOString().slice(0, 10)
+                    : "-"}
+                </td>
+                <td>
+                  <div className="form-group">
+                    {/* when clicking the update button, it will trigger the onClick method
                     and inside the onClick method, we will pass in the trip.id */}
-                           <button
-                             onClick={() => this.onTripUpdate(trip.id)}
-                             className="btn btn-success"
-                           >
-                             Update
-                           </button>
-                         </div>
-                       </td>
-                     </tr>
-                   ))
-                 }
-               </tbody>
-             </table>
-           );
-         }
+                    <button
+                      onClick={() => this.onTripUpdate(trip.id)}
+                      className="btn btn-success"
+                    >
+                      Update
+                    </button>
+                    <button
+                      onClick={() => this.onTripDelete(trip.id)}
+                      className="btn btn-danger"
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </td>
+              </tr>
+            ))
+          }
+        </tbody>
+      </table>
+    );
+  }
 
-         // a class based component always has a render method which returns the JSX
-         render() {
-           // if we haven't got any data yet, we don't want to display an empty table
-           // if the state of the loading is true, meaning data haven't returned yet so we need to let the user know that the data is still loading
-           // otherwise, we need to create a table to display the info
-           let content = this.state.loading ? (
-             <p>
-               <em>Loading...</em>
-             </p>
-           ) : (
-             // pass the this.state.trips array
-             this.renderAllTripsTable(this.state.trips)
-           );
+  // a class based component always has a render method which returns the JSX
+  render() {
+    // if we haven't got any data yet, we don't want to display an empty table
+    // if the state of the loading is true, meaning data haven't returned yet so we need to let the user know that the data is still loading
+    // otherwise, we need to create a table to display the info
+    let content = this.state.loading ? (
+      <p>
+        <em>Loading...</em>
+      </p>
+    ) : (
+      // pass the this.state.trips array
+      this.renderAllTripsTable(this.state.trips)
+    );
 
-           return (
-             <div>
-               <h1>All trips</h1>
-               {content}
-             </div>
-           );
-         }
-       }
+    return (
+      <div>
+        <h1>All trips</h1>
+        {content}
+      </div>
+    );
+  }
+}
